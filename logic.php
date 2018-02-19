0aca105ec47f87f26c12d3c1c1e35f8cbd415c1a
Anup Shetye <anup.shetye@gmail.com>
2018-02-19 14:01:23 -0500

    Update logic.php

--- logic.php
+++ logic.php
@@ -1,27 +1,24 @@
 <?php
-	
-	$symbol = array('!', '@', '#', '$', '^', '&', '*');
-	if($wordlist = file("wordlist.txt", FILE_IGNORE_NEW_LINES)) {
-		
-		$password = "";
-		$count = (empty($_POST["num_of_word"]) ? 4 : $_POST["num_of_word"]);
-		for ($i = 0; $i < $count; $i++) {
-			$word = $wordlist[rand(0, 4999)];
-			if (isset($_POST["add_number"])) {
-				$word = ucfirst($word);
-			}
-			if ($i != 0) {
-				$password .= ("-".$word);
-			} else {
-				$password .= $word;
-			}
-				
-		}
-		if(isset($_POST["add_number"])) {
-				$password .= rand(0, 9);
-		}
-		if(isset($_POST["add_symbol"])) {
-				$password .= $symbol[array_rand($symbol,1)];
-		}
-			
-	}
+$symbol = ['!', '@', '#', '$', '^', '%'];
+if ($wordlist = file("wordlist.txt", FILE_IGNORE_NEW_LINES)) {
+    $password = "";
+    $count = (empty($_POST["num_of_word"]) ? 4 : $_POST["num_of_word"]);
+    for ($index = 0; $index < $count; $index++) {
+        $word = $wordlist[rand(0, 4999)];
+        if ($index != 0) {
+            $password .= ("-" . $word);
+        } else {
+            $password .= $word;
+        }
+    }
+    if (isset($_POST["add_number"])) {
+        $password .= rand(0, 9);
+    }
+    if (isset($_POST["add_symbol"])) {
+        foreach ($symbol as &$value) {
+            if ($value == $_POST["add_symbol"]){
+                $password .= $value;
+            }
+        }
+    }
+}
