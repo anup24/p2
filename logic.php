4eb50d8dfb9f1cdf777723e2189e315a1c0852a8
Anup Shetye <anup.shetye@gmail.com>
2018-02-20 23:11:42 -0500

    Update logic.php

--- logic.php
+++ logic.php
@@ -1,8 +1,20 @@
 <?php
-$symbol = ['!', '@', '#', '$', '^', '%'];
-if ($wordlist = file("wordlist.txt", FILE_IGNORE_NEW_LINES)) {
+
+if (empty($_POST["num_of_word"])) {
+    $message = "";
+
+} else if (!is_numeric($_POST["num_of_word"])) {
+    $message = "Please enter a valid number for number of words to generate your password.";
+
+} else if (is_numeric($_POST["num_of_word"]) && ($_POST["num_of_word"] < 1 || $_POST["num_of_word"] > 8)) {
+    $message = "Please enter a valid number between 1 and 8 for number of words.";
+
+} else if ($wordlist = file("wordlist.txt", FILE_IGNORE_NEW_LINES)) {
+
+    $symbol = ['!', '@', '#', '$', '^', '%'];
     $password = "";
-    $count = (empty($_POST["num_of_word"]) ? 4 : $_POST["num_of_word"]);
+    $count = $_POST["num_of_word"];
+
     for ($index = 0; $index < $count; $index++) {
         $word = $wordlist[rand(0, 4999)];
         if ($index != 0) {
