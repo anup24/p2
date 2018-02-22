bcd2940bc92ef3c7bb4e24ddef58b7fabc41de40
Anup Shetye <anup.shetye@gmail.com>
2018-02-21 22:11:31 -0500

    Update logic.php

--- logic.php
+++ logic.php
@@ -1,15 +1,6 @@
 <?php
 
-if (empty($_POST["num_of_word"])) {
-    $message = "";
-
-} else if (!is_numeric($_POST["num_of_word"])) {
-    $message = "Please enter a valid number for number of words to generate your password.";
-
-} else if (is_numeric($_POST["num_of_word"]) && ($_POST["num_of_word"] < 1 || $_POST["num_of_word"] > 8)) {
-    $message = "Please enter a valid number between 1 and 8 for number of words.";
-
-} else if ($wordlist = file("wordlist.txt", FILE_IGNORE_NEW_LINES)) {
+if ($wordlist = file("wordlist.txt", FILE_IGNORE_NEW_LINES)) {
 
     $symbol = ['!', '@', '#', '$', '^', '%'];
     $password = "";
